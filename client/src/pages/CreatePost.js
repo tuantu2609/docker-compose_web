3a837b5a864226d07e6d8e9efa1c6d7be1f5cf3b
tuantu2609 <tuantums10@gmail.com>
2024-10-28 21:30:59 +0700

    docker-compose is the best

---
+++ client/src/pages/CreatePost.js
@@ -0,0 +1,67 @@
+import React, { useContext, useEffect } from "react";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import axios from "axios";
+import { useNavigate} from 'react-router-dom';
+import { AuthContext } from "../helpers/AuthContext";
+
+
+function CreatePost() {
+  const { authState } = useContext(AuthContext);
+  let navigate = useNavigate();
+  const initialValues = {
+        title: "",
+        postText: "",
+  };
+
+  useEffect(() => {
+    if (!localStorage.getItem("accessToken")) {
+      navigate("/login");
+    }
+  }, []);
+
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required('Điền cái title vào ml !!!'),
+    postText: Yup.string().required(),
+  });
+
+  const onSubmit = (data) => {
+    axios
+      .post("http://localhost:3001/posts", data, {
+        headers: { accessToken: localStorage.getItem("accessToken") },
+      })
+      .then((response) => {
+        navigate("/");
+      });
+  };
+
+  return (
+    <div className='createPostPage'>
+
+      <Formik initialValues={initialValues} 
+              onSubmit={onSubmit} 
+              validationSchema={validationSchema}
+      >
+        <Form className='formContainer'>
+          <label>Title: </label>
+          <ErrorMessage name='title' component='span'/>
+          <Field 
+          id="inputCreatePost" 
+          name="title" 
+          placeholder="(Ex. Title....)" />
+          <label>Post:</label>
+          <ErrorMessage name='postText' component='span'/>
+          <Field 
+          id="inputCreatePost" 
+          name="postText" 
+          placeholder="(Ex. Title....)" />
+
+          <button type="submit">Create Post</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
+
+export default CreatePost
+
---
+++ client/src/pages/Post.js
@@ -0,0 +1,132 @@
+import React, { useEffect, useState, useContext } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { AuthContext } from '../helpers/AuthContext';
+import { useNavigate} from 'react-router-dom';
+
+
+function Post() {
+  let { id } = useParams();
+  const [postObject, setPostObject] = useState({});
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState("");
+  const { authState } = useContext(AuthContext);
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/posts/byId/${id}`).then((response) => {
+      setPostObject(response.data);
+    });
+
+    axios.get(`http://localhost:3001/comments/${id}`).then((response) => {
+      setComments(response.data);
+    });
+  }, [id]);
+
+  const addComment = () => {
+    axios
+      .post(
+        "http://localhost:3001/comments",
+        {
+          commentBody: newComment,
+          PostId: id,
+        },
+        {
+          headers: {
+            accessToken: localStorage.getItem("accessToken"),
+          },
+        }
+      )
+      .then((response) => {
+        if (response.data.error) {
+          console.log(response.data.error);
+        } else {
+          const commentToAdd = { commentBody: newComment, userName: response.data.userName };
+          setComments([...comments, commentToAdd]);
+          setNewComment("");
+        }
+      });
+  };
+
+  const deleteComment = (id) => {
+    axios
+      .delete(`http://localhost:3001/comments/${id}`, {
+        headers: { accessToken: localStorage.getItem("accessToken") },
+      })
+      .then(() => {
+        setComments(
+          comments.filter((val) => {
+            return val.id !== id;
+          })
+        );
+      });
+  };
+
+  const deletePost = (id) => {
+    axios
+      .delete(`http://localhost:3001/posts/${id}`, {
+        headers: { accessToken: localStorage.getItem("accessToken") },
+      })
+      .then(() => {
+        navigate("/");
+      });
+  };
+
+  return (
+    <div className="postPage">
+      <div className="leftSide">
+        <div className="post" id="individual">
+          <div className="title"> {postObject.title} </div>
+          <div className="body">{postObject.postText}</div>
+          <div className="footer">{postObject.username}
+            {authState.username === postObject.username && (
+              <button
+                onClick={() => {
+                  deletePost(postObject.id);
+                }}
+              >
+                {" "}
+                Delete Post
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="rightSide">
+        <div className="addCommentContainer">
+          <input
+            type="text"
+            placeholder="Comment..."
+            autoComplete="off"
+            value={newComment}
+            onChange={(event) => {
+              setNewComment(event.target.value);
+            }}
+          />
+          <button onClick={addComment}> Add Comment</button>
+        </div>
+        <div className="listOfComments">
+          {comments.map((comment, key) => {
+            return (
+              <div key={key} className="comment">
+                {comment.commentBody}
+                <label> Username: {comment.userName}</label>
+                {authState.username === comment.userName && (
+                  <button
+                    onClick={() => {
+                      deleteComment(comment.id);
+                    }}
+                  >
+                    X
+                  </button>
+                )}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Post;
